78cff81adbd741be2bd3e16c1530be117e2e8039
山外小楼夜听雨 <62926259+blindSpoter01@users.noreply.github.com>
2020-09-23 20:43:15 +0800

    Update op.py
    用pickle存储相应数据，简单

--- op.py
+++ op.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import pickle
 import sys
 import paramiko
 import os
@@ -11,12 +12,12 @@ sys.setdefaultencoding('utf-8')
 
 
 class Node:
-    def __init__(self, content, name):
-        self.content = content
-        self.name = name
-        self.index = 0
+    def __init__(self, functionName, nodeName):
+        self.functionName = functionName
+        self.nodeName = nodeName
+        self.index = -1
 
-    def initIndex(self, index):
+    def init_index(self, index):
         self.index = index
 
 
@@ -51,7 +52,7 @@ def check_local_dir(local_dir_name):
         os.makedirs(local_dir_name)
 
 
-def download(remote_dir_before, remote_dir_after, local_dir_before, local_dir_after):
+def download_java_file(remote_dir_before, remote_dir_after, local_dir_before, local_dir_after):
     # 服务器连接信息
     host_name = '114.212.87.13'
     user_name = 'csy'
@@ -75,56 +76,16 @@ def download(remote_dir_before, remote_dir_after, local_dir_before, local_dir_af
     t.close()
 
 
-def cmdDoxygen(index):
-    os.system("doxygen D:\doxygen\Doxyfile" + index)
+def cmd_doxygen(index):
+    os.system("doxygen E:\pythonProject\dataProcess\Doxyfile" + index)
 
 
-def dic2list(dic):
-    r = []
-    for i in dic:
-        r.append(i)
-    for i in dic:
-        r[i] = dic[i]
-    return r
-
-
-def save_by_np(index, dicList):
-    dir = r"adjacencyMatrixDicList"
-    for i in range(len(dicList)):
-        path = dir + "/" + str(i)
-        check_local_dir(path)
-        ad = dic2list(dicList[i])
-        res = np.array(ad)
-        np.save(path + r"/" + index, res)
-
-
-nodeListBeforeList = []
-nodeListAfterList = []
-# nodeDic = {}
-# nodeContentDic = {}
-adjacencyMatrixDicBeforeList = []
-adjacencyMatrixDicAfterList = []
-print("start")
-for i in range(1):
+def draw():
+    nodeContentDic = {}
     nodeList = []
     nodeDic = {}
-    nodeContentDic = {}
     adjacencyMatrixDic = {}
-    remote_dir_before = '/tmp/pycharm_project_805/javaTest/' + str(i) + '/before'
-    remote_dir_after = '/tmp/pycharm_project_805/javaTest/' + str(i) + '/after'
-    # 本地文件存放路径（绝对路径或者相对路径都可以）
-    local_dir_before = 'downloadNow/before'
-    local_dir_after = 'downloadNow/after'
-    print("download start #######################")
-    download(remote_dir_before, remote_dir_after, local_dir_before, local_dir_after)
-    print("download end #######################")
-
-    print("cmd before start #######################")
-    cmdDoxygen("Before")
-    print("cmd before end ######################")
-
-    print("draw before start")
-    path = "D:\\doxygen\\doxygen\\workspace\\html"
+    path = "D:/doxygen/workspace/html"
     filesList = os.listdir(path)
     for file in filesList:
         if not file.endswith("cgraph.dot") or not file.endswith("icgraph.dot"):
@@ -133,7 +94,6 @@ for i in range(1):
         with open(fileName) as f:
             dot_graph = f.read()
             dotGrapgList = dot_graph.split("\n")
-
         for s in dotGrapgList:
             s = s.strip()
             if s.startswith("Node"):
@@ -145,7 +105,7 @@ for i in range(1):
                     name = s[:nameIndex]
                     if content not in nodeContentDic:
                         tmpNode = Node(content, name)
-                        tmpNode.initIndex(len(nodeList))
+                        tmpNode.init_index(len(nodeList))
                         nodeList.append(tmpNode)
                         nodeDic[name] = tmpNode
                         nodeContentDic[content] = tmpNode
@@ -167,68 +127,56 @@ for i in range(1):
                 else:
                     if nodeDic[nodeSecond].index not in adjacencyMatrixDic[nodeDic[nodeFirst].index]:
                         adjacencyMatrixDic[nodeDic[nodeFirst].index].append(nodeDic[nodeSecond].index)
-    nodeListBeforeList.append(nodeList)
-    adjacencyMatrixDicBeforeList.append(adjacencyMatrixDic)
-    print("draw before end")
+    return nodeList, adjacencyMatrixDic
 
-    nodeList = []
-    nodeDic = {}
-    nodeContentDic = {}
-    adjacencyMatrixDic = {}
-    print("cmd after start #######################")
-    cmdDoxygen("After")
-    print("cmd after end ######################")
 
-    print("draw after start")
-    path = "D:\\doxygen\\doxygen\\workspace\\html"
-    filesList = os.listdir(path)
-    for file in filesList:
-        if not file.endswith("cgraph.dot") or not file.endswith("icgraph.dot"):
-            continue
-        fileName = path + "\\" + file
-        with open(fileName) as f:
-            dot_graph = f.read()
-            dotGrapgList = dot_graph.split("\n")
+def save(nodeList, adjacencyMatrixDic, projectName, index, beforeOrAfter):
+    abpath = 'E:/pythonProject/dataProcess/process/' + projectName + '/commit' + str(index) + '/'
+    check_local_dir(abpath)
+    nodeListFilePath = abpath + '/' + beforeOrAfter + 'NodeList' + '.data'
+    adjacencyMatrixDicFilePath = abpath + '/' + beforeOrAfter + 'AdjacencyMatrixDic' + '.data'
+    with open(nodeListFilePath, 'wb') as file:
+        pickle.dump(nodeList, file)
+    with open(adjacencyMatrixDicFilePath, 'wb') as file:
+        pickle.dump(adjacencyMatrixDic, file)
 
-        for s in dotGrapgList:
-            s = s.strip()
-            if s.startswith("Node"):
-                if "label=\"" in s:
-                    index1 = s.index("label=\"")
-                    index2 = s.index(",")
-                    content = s[index1 + 7:index2 - 1].replace("\\l", "")
-                    nameIndex = s.index(" ")
-                    name = s[:nameIndex]
-                    if content not in nodeContentDic:
-                        tmpNode = Node(content, name)
-                        tmpNode.initIndex(len(nodeList))
-                        nodeList.append(tmpNode)
-                        nodeDic[name] = tmpNode
-                        nodeContentDic[content] = tmpNode
-                    else:
-                        nodeDic[name] = nodeContentDic[content]
-        for s in dotGrapgList:
-            s = s.strip()
-            if "->" in s:
-                sList = s.split(" ")
-                nodeFirst = sList[0]
-                nodeSecond = sList[2]
-                if not nodeDic[nodeFirst].index in adjacencyMatrixDic:
-                    adjacencyMatrixDic[nodeDic[nodeFirst].index] = []
-                if "dir=\"back\"" in s:
-                    if not nodeDic[nodeSecond].index in adjacencyMatrixDic:
-                        adjacencyMatrixDic[nodeDic[nodeSecond].index] = []
-                    if nodeDic[nodeFirst].index not in adjacencyMatrixDic[nodeDic[nodeSecond].index]:
-                        adjacencyMatrixDic[nodeDic[nodeSecond].index].append(nodeDic[nodeFirst].index)
-                else:
-                    if nodeDic[nodeSecond].index not in adjacencyMatrixDic[nodeDic[nodeFirst].index]:
-                        adjacencyMatrixDic[nodeDic[nodeFirst].index].append(nodeDic[nodeSecond].index)
-    nodeListAfterList.append(nodeList)
-    adjacencyMatrixDicAfterList.append(adjacencyMatrixDic)
-    print("draw after end ")
-# print(adjacencyMatrixDic)
 
-print "save start"
-save_by_np("before", adjacencyMatrixDicBeforeList)
-save_by_np("after", adjacencyMatrixDicAfterList)
-print "save end"
+with open('project_name_dic.data', 'r') as file:
+    project_name_dic = pickle.load(file)
+# project_name_dic = {"openstack-infra_gerrit": 1}
+for projectName in project_name_dic:
+    projectCommitNum = project_name_dic[projectName]
+    for i in range(projectCommitNum):
+        print "download start #######################"
+        remote_dir_before = '/tmp/pycharm_project_136/javaFile/' + projectName + '/commit' + str(i) + '/before'
+        remote_dir_after = '/tmp/pycharm_project_136/javaFile/' + projectName + '/commit' + str(i) + '/after'
+        # remote_dir_before = '/tmp/pycharm_project_136/javaFile/' + projectName + '/commit5' + '/before'
+        # remote_dir_after = '/tmp/pycharm_project_136/javaFile/' + projectName + '/commit5' + '/after'
+        local_dir_before = 'downloadNow/before'
+        local_dir_after = 'downloadNow/after'
+        download_java_file(remote_dir_before, remote_dir_after, local_dir_before, local_dir_after)
+        print "download end #######################"
+
+        print "cmd before start #######################"
+        cmd_doxygen("Before")
+        print "cmd before end ######################"
+
+        print "draw before start ######################"
+        nodeList, adjacencyMatrixDic = draw()
+        print "draw before start ######################"
+
+        print "save before start ######################"
+        save(nodeList, adjacencyMatrixDic, projectName, i, "before")
+        print "save before end ######################"
+
+        print "cmd after start #######################"
+        cmd_doxygen("After")
+        print "cmd after end ######################"
+
+        print "draw after start ######################"
+        nodeList, adjacencyMatrixDic = draw()
+        print "draw after start ######################"
+
+        print "save after start ######################"
+        save(nodeList, adjacencyMatrixDic, projectName, i, "after")
+        print "save after end ######################"
